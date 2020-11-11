d6b5cb5dcd6842f1cde98f913dcbdd8d87b56122
Kenta Nakamori <kenta@MacBook-Pro.local>
2020-11-11 23:09:16 +0900

    Error: Invalid salt edit

--- project/config.py
+++ project/config.py
@@ -1,6 +1,7 @@
 import os
 
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+# SQLALCHEMY_DATABASE_URI = 'postgresql://kenta:kenta@localhost:5432/hogehoge'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 DEBUG = True
 SECRET_KEY = 'secret key'
